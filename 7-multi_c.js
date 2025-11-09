2741f85ace953d5447d2212876f2171a3d1d3110
Roberta Smith <efyarobbi@gmail.com>
2025-11-09 21:07:03 +0000

    Add the rephrased print

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -2,13 +2,11 @@ const count = parseInt(process.argv[2]);
 
 if (isNaN(count)) {
   console.log("Missing number of occurrences");
-} else {
-  if (count > 0) {
-    let output = "";
-    for (let i = 0; i < count; i++) {
-      output += "C is fun\n";
-    }
-    console.log(output);
+} else if (count > 0) {
+  let output = "";
+  for (let i = 0; i < count; i++) {
+    output += "C is fun\n";
   }
+  console.log(output);
 }
 
